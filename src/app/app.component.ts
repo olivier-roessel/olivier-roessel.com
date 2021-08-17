19836e4ef3f4be1aef1752aff4e94a5f2b6f1aeb
Olivier Roessel <olivier.roessel@outlook.com>
2021-08-17 22:36:37 +0200

    Microsoft Clarity cookies added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,7 +18,7 @@ export class AppComponent {
   addClarity() {
     let script = document.createElement('script');
     script.type = "text/javascript";
-    script.innerHTML = "(function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }; t = l.createElement(r); t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y); })(window, document, 'clarity', 'script', '4o9o9rn8yw');";
+    script.innerHTML = "(function (c, l, a, r, i, t, y) { c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) }; t = l.createElement(r); t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i; y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y); })(window, document, 'clarity', 'script', '4o9o9rn8yw'); window.clarity('consent');";
     document.head.prepend(script);
   }
 }
