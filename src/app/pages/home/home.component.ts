78f529fc408844b389b864a70316e5403456ab56
Olivier Roessel <olivier.roessel@outlook.com>
2023-05-11 16:12:58 +0200

    Various UX improvements

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,9 +10,11 @@ export class HomeComponent implements AfterViewInit {
   @ViewChild('rowSocial') el1!: ElementRef;
   pos1 = { top: 0, left: 0, x: 0, y: 0 };
   active1 = false;
+  speed1 = 0;
   @ViewChild('rowLiveroom') el2!: ElementRef;
   pos2 = { top: 0, left: 0, x: 0, y: 0 };
   active2 = false;
+  speed2 = 0;
 
   constructor(private titleService: Title) {
     this.titleService.setTitle("Olivier Roessel");
@@ -31,6 +33,7 @@ export class HomeComponent implements AfterViewInit {
       this.el2.nativeElement.scroll({ left: this.pos2.left + 270, top: 0, behavior: 'smooth' });
     });
     this.el1.nativeElement.addEventListener('mousedown', (e: any) => {
+      this.speed1 = 0;
       this.pos1 = {
         left: this.el1.nativeElement.scrollLeft,
         top: this.el1.nativeElement.scrollTop,
@@ -40,6 +43,7 @@ export class HomeComponent implements AfterViewInit {
       this.active1 = true;
     });
     this.el2.nativeElement.addEventListener('mousedown', (e: any) => {
+      this.speed2 = 0;
       this.pos2 = {
         left: this.el2.nativeElement.scrollLeft,
         top: this.el2.nativeElement.scrollTop,
@@ -49,20 +53,38 @@ export class HomeComponent implements AfterViewInit {
       this.active2 = true;
     });
     document.addEventListener('mouseup', () => {
-      this.active1 = false;
-      this.active2 = false;
+      if (this.active1) {
+        this.active1 = false;
+        this.el1.nativeElement.scroll({ left: this.el1.nativeElement.scrollLeft - (this.speed1 * 8), top: 0, behavior: 'smooth', easing: 'ease-out' });
+      }
+      if (this.active2) {
+        this.active2 = false;
+        this.el2.nativeElement.scroll({ left: this.el2.nativeElement.scrollLeft - (this.speed2 * 8), top: 0, behavior: 'smooth', easing: 'ease-out' });
+      }
     });
     document.addEventListener('mousemove', (e: any) => {
       if (this.active1) {
         const dx = e.clientX - this.pos1.x;
         const dy = e.clientY - this.pos1.y;
+        const oldPos = this.el1.nativeElement.scrollLeft;
+        const newPos = this.pos1.left - dx;
+        this.speed1 = oldPos - newPos;
         this.el1.nativeElement.scrollTop = this.pos1.top - dy;
         this.el1.nativeElement.scrollLeft = this.pos1.left - dx;
+        setTimeout(() => {
+          this.speed1 = 0;
+        }, 100);
       } else if (this.active2) {
         const dx = e.clientX - this.pos2.x;
         const dy = e.clientY - this.pos2.y;
+        const oldPos = this.el2.nativeElement.scrollLeft;
+        const newPos = this.pos2.left - dx;
+        this.speed2 = oldPos - newPos;
         this.el2.nativeElement.scrollTop = this.pos2.top - dy;
         this.el2.nativeElement.scrollLeft = this.pos2.left - dx;
+        setTimeout(() => {
+          this.speed2 = 0;
+        }, 100);
       }
     });
   }
